fa449b95c2008bb43da5f4ab1ccbc3779637b350
Douglas Volcato <douglasvolcato@gmail.com>
2022-11-14 23:31:09 -0300

    feat: add entity interfaces

---
+++ src/protocols/entities/game.ts
@@ -0,0 +1,9 @@
+export interface Game {
+  title: string;
+  coverImageUrl: string;
+  description: string;
+  year: number;
+  imdbScore: number;
+  trailerYouTubeUrl: string;
+  gameplayYouTubeUrl: string;
+}
---
+++ src/protocols/entities/user.ts
@@ -0,0 +1,7 @@
+export interface User {
+  name: string;
+  email: string;
+  password: string;
+  cpf: number;
+  isAdmin: boolean;
+}
