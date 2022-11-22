de3d6c47b8ec2ae9e55b1dbbd83054b8cbbfc602
Douglas Volcato <douglasvolcato@gmail.com>
2022-11-22 19:45:31 -0300

    feat: add mocked database

---
+++ src/infra/database/mocked.ts
@@ -0,0 +1,7 @@
+import { Game } from 'src/domain/entities/game-entity';
+import { Profile } from 'src/domain/entities/profile-entity';
+import { User } from 'src/domain/entities/user-entity';
+
+export const arrUsers: User[] = [];
+export const arrProfiles: Profile[] = [];
+export const arrGames: Game[] = [];
