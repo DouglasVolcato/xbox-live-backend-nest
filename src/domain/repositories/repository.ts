be4cf8755f9cebec744cb86ee7c0f9885ffdabe4
Douglas Volcato <douglasvolcato@gmail.com>
2022-11-22 19:45:11 -0300

    refaactor: add new input types for repository interface

--- src/domain/repositories/repository.ts
+++ src/domain/repositories/repository.ts
@@ -1,14 +1,14 @@
-import { GameDto } from '../dtos/game-dto';
-import { ProfileDto } from '../dtos/profile-dto';
-import { UserDto } from '../dtos/user-dto';
 import { Game } from '../entities/game-entity';
 import { Profile } from '../entities/profile-entity';
 import { User } from '../entities/user-entity';
 
 export interface Repository {
   create(body: Game | Profile | User): Promise<Game | Profile | User>;
-  getOne(id: string): Promise<Game | Profile | User>;
-  getAll(): Promise<Game[] | Profile[] | User[]>;
-  update(body: GameDto | ProfileDto | UserDto): Promise<Game | Profile | User>;
-  delete(id: string): Promise<Game | Profile | User>;
+  getOne(id: string): Promise<Game | Profile | User | void>;
+  getAll(): Promise<Game[] | Profile[] | User[] | []>;
+  update(
+    body: Game | Profile | User,
+    id: string,
+  ): Promise<Game | Profile | User | void>;
+  delete(id: string): Promise<Game | Profile | User | void>;
 }
