de3fa3132fd57035cc2c483b48dc2cf94e6ce695
Douglas Volcato <douglasvolcato@gmail.com>
2022-11-22 18:39:27 -0300

    refactor: create a single body http response

--- src/domain/http/http-response.ts
+++ src/domain/http/http-response.ts
@@ -2,9 +2,15 @@ import { Game } from '../entities/game-entity';
 import { Profile } from '../entities/profile-entity';
 import { User } from '../entities/user-entity';
 
+interface Message {
+  message: string;
+}
+
+interface Token {
+  token: string;
+}
+
 export interface HttpRsponse {
   statusCode: number;
-  body?: Game | User | Profile | Game[] | User[] | Profile[];
-  message?: string;
-  token?: string;
+  body: Game | User | Profile | Game[] | User[] | Profile[] | Message | Token;
 }
