6f2b78a7603eef9065253e446214e11b1f8c0991
Douglas Volcato <douglasvolcato@gmail.com>
2022-11-22 00:29:12 -0300

    refactor: create a unique repository interface

---
+++ src/domain/repository.ts
@@ -0,0 +1,14 @@
+import { GameDto } from './dtos/game-dto';
+import { ProfileDto } from './dtos/profile-dto';
+import { UserDto } from './dtos/user-dto';
+import { Game } from './entities/game';
+import { Profile } from './entities/profile';
+import { User } from './entities/user';
+
+export interface Repository {
+  create(body: Game | Profile | User): Promise<Game | Profile | User>;
+  getOne(id: string): Promise<Game | Profile | User>;
+  getAll(): Promise<Game[] | Profile[] | User[]>;
+  update(body: GameDto | ProfileDto | UserDto): Promise<Game | Profile | User>;
+  delete(id: string): Promise<Game | Profile | User>;
+}
