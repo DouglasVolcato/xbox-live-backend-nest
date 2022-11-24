a5c3263a99f5e4cfb77d6b2808714736184e3d94
Douglas Volcato <douglasvolcato@gmail.com>
2022-11-24 00:50:47 -0300

    feat: add RequestBodyDto interface

---
+++ src/domain/dtos/request-body-dto.ts
@@ -0,0 +1,21 @@
+export interface RequestBodyDto {
+  headers?: {
+    authorization?: string;
+  };
+  params?: { id?: string };
+  body?: {
+    coverImageUrl?: string;
+    description?: string;
+    year?: number;
+    imdbScore?: number;
+    trailerYouTubeUrl?: string;
+    gameplayYouTubeUrl?: string;
+    title?: string;
+    imageUrl?: string;
+    name?: string;
+    email?: string;
+    password?: string;
+    cpf?: number;
+    isAdmin?: boolean;
+  };
+}
