226334d88aee46543c3f81e133252e1855e6b2ba
Douglas Volcato <douglasvolcato@gmail.com>
2022-11-24 00:48:45 -0300

    refactor: delete unused code

--- src/data/abstract/user/getOne-user-interface.ts
+++
@@ -1,5 +0,0 @@
-import { UserEntityInterface } from 'src/domain/entities/user-entity-interface';
-
-export interface GetOneUserUseCaseInterface {
-  execute(id: string): Promise<UserEntityInterface | void>;
-}
