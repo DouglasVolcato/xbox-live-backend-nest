4ffd7f75b9e911325b72d84cfd6adf9365b3f735
Douglas Volcato <douglasvolcato@gmail.com>
2022-11-25 18:33:14 -0300

    feat: add error generators

--- src/presentation/controllers/user/user-controller.ts
+++ src/presentation/controllers/user/user-controller.ts
@@ -6,7 +6,8 @@ import {
   UpdateUserUseCaseInterface,
   UserControllerInterface,
   HttpRequest,
-  HttpResponse,GetAllUsersUseCaseInterface
+  HttpResponse,
+  GetAllUsersUseCaseInterface,
 } from './interface-imports';
 import { HttpResponseHandler } from 'src/utils/handlers/http/http-response-handler';
 
@@ -25,8 +26,8 @@ export class UserController implements UserControllerInterface {
     const created = await this.createUserUseCase.execute(body);
 
     if (created) {
-      const http = new HttpResponseHandler({ message: 'User created.' }, 200);
-      return http.response();
+      const http = new HttpResponseHandler({ message: 'User created.' });
+      return http.created();
     }
   }
 
@@ -35,8 +36,8 @@ export class UserController implements UserControllerInterface {
     const foundUser = await this.getOneUserByEmailUseCase.execute(email);
 
     if (foundUser) {
-      const http = new HttpResponseHandler(foundUser, 200);
-      return http.response();
+      const http = new HttpResponseHandler(foundUser);
+      return http.ok();
     }
   }
 
@@ -45,8 +46,8 @@ export class UserController implements UserControllerInterface {
     const foundUser = await this.getOneUserByIdUseCase.execute(id);
 
     if (foundUser) {
-      const http = new HttpResponseHandler(foundUser, 200);
-      return http.response();
+      const http = new HttpResponseHandler(foundUser);
+      return http.ok();
     }
   }
 
@@ -54,8 +55,8 @@ export class UserController implements UserControllerInterface {
     const foundUsers = await this.getAllUsersUseCase.execute();
 
     if (foundUsers) {
-      const http = new HttpResponseHandler(foundUsers, 200);
-      return http.response();
+      const http = new HttpResponseHandler(foundUsers);
+      return http.ok();
     }
   }
 
@@ -65,8 +66,8 @@ export class UserController implements UserControllerInterface {
     const updated = await this.updateUserUseCase.execute(body, id);
 
     if (updated) {
-      const http = new HttpResponseHandler({ message: 'User updated.' }, 200);
-      return http.response();
+      const http = new HttpResponseHandler({ message: 'User updated.' });
+      return http.ok();
     }
   }
 
@@ -75,8 +76,8 @@ export class UserController implements UserControllerInterface {
     const deleted = await this.deleteUserUseCase.execute(id);
 
     if (deleted) {
-      const http = new HttpResponseHandler({ message: 'User deleted.' }, 200);
-      return http.response();
+      const http = new HttpResponseHandler({ message: 'User deleted.' });
+      return http.ok();
     }
   }
 }
