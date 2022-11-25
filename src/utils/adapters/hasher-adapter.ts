ddd366dfa5208533387e199214659b04869c6f72
Douglas Volcato <douglasvolcato@gmail.com>
2022-11-25 01:30:26 -0300

    refactor: deconstruct bcrypt methods

--- src/utils/adapters/hasher-adapter.ts
+++ src/utils/adapters/hasher-adapter.ts
@@ -1,11 +1,12 @@
 import { HasherInterface } from '../abstract/hasher-interface';
-import bcrypt from 'bcrypt';
+import { hash, compare } from 'bcrypt';
 
 export class HasherAdapter implements HasherInterface {
   async hash(password: string, saltRounds: number): Promise<string> {
-    return await bcrypt.hash(password, saltRounds);
+    return await hash(password, saltRounds);
   }
+
   async compare(password: string, hashedPassword: string): Promise<boolean> {
-    return await bcrypt.compare(password, hashedPassword);
+    return await compare(password, hashedPassword);
   }
 }
