f96787516c9082314e1caa3a6d0fab14bddf8f2b
Douglas Volcato <douglasvolcato@gmail.com>
2022-11-24 21:13:05 -0300

    feat: implement nest UserController

--- src/main/nest-adapter/user/user.controller.ts
+++ src/main/nest-adapter/user/user.controller.ts
@@ -1,4 +1,55 @@
-import { Controller } from '@nestjs/common';
+import { HttpResponse } from 'src/domain/http/http-response';
+import { makeUserControllerFactory } from 'src/main/factories/user-controller-factory';
+import { HttpRequestHandler } from 'src/utils/handlers/http/http-request-handler';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+} from '@nestjs/common';
+import { UserDto } from 'src/domain/dtos/user-dto';
+const user = makeUserControllerFactory();
 
 @Controller('user')
-export class UserController {}
+export class UserController {
+  @Post('create-user')
+  async create(@Body() body: UserDto) {
+    const http = new HttpRequestHandler({ body });
+    return await user.update(http.request());
+  }
+
+  @Get('get-all-users')
+  async getAll(): Promise<HttpResponse> {
+    return await user.getAll();
+  }
+
+  @Get('get-user-by-id/:id')
+  async getOneById(@Param() id: string): Promise<HttpResponse> {
+    const http = new HttpRequestHandler({ params: { id } });
+    return await user.getOneById(http.request());
+  }
+
+  @Get('get-user-by-email')
+  async getOneByEmail(@Body() body: any): Promise<HttpResponse> {
+    const http = new HttpRequestHandler({ body });
+    return await user.getOneByEmail(http.request());
+  }
+
+  @Delete('delete-user/:id')
+  async delete(@Param() id: string): Promise<HttpResponse> {
+    const http = new HttpRequestHandler({ params: { id } });
+    return await user.delete(http.request());
+  }
+
+  @Patch('update-user/:id')
+  async update(
+    @Param(':id') id: string,
+    @Body() body: UserDto,
+  ): Promise<HttpResponse> {
+    const http = new HttpRequestHandler({ params: { id }, body });
+    return await user.update(http.request());
+  }
+}
