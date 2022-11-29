351afe9947581e329f2c2b6996f48feebcf27a2a
Douglas Volcato <douglasvolcato@gmail.com>
2022-11-28 23:18:28 -0300

    refactor: move nest-app-module to nest-adapter folder

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { AppModule } from './main/nest-adapter/modules/app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
---
+++ src/main/nest-adapter/modules/app.module.ts
@@ -0,0 +1,10 @@
+import { Module } from '@nestjs/common';
+import { ProfileModule } from './profile.module';
+import { UserModule } from './user.module';
+import { GameModule } from './game.module';
+import { ApiTestModule } from './apiTest.module';
+
+@Module({
+  imports: [ApiTestModule, UserModule, ProfileModule, GameModule],
+})
+export class AppModule {}
