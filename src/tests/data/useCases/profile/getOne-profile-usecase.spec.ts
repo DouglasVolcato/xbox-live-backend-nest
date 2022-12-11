324e7f9a53946b67d382e276b0ee386c76c938f7
Douglas Volcato <douglasvolcato@gmail.com>
2022-12-11 01:58:10 -0300

    test: implement GetOneProfileUseCase unity tests

--- src/tests/data/useCases/profile/getOne-profile-usecase.spec.ts
+++ src/tests/data/useCases/profile/getOne-profile-usecase.spec.ts
@@ -2,6 +2,8 @@ import { makeError } from '../../../test-utils/errors/make-error';
 import { GetOneProfileUseCase } from '../../../../data/useCases/profile/getOne-profile-usecase';
 import { fakeProfile } from '../../../test-utils/fake-entities/fake-profile';
 import { ProfileRepositoryStub } from '../../../test-utils/stubs/repositories/profile-repository-stub';
+import { InvalidParamError } from '../../../../utils/errors';
+import { fakeUser } from '../../../test-utils/fake-entities/fake-user';
 
 interface SutTypes {
   profileRepositoryStub: ProfileRepositoryStub;
@@ -15,10 +17,10 @@ function makeSut(): SutTypes {
 }
 
 describe('GetOneProfileUseCase', () => {
-  test('Ensure ProfileReposiory is called with correct value.', async () => {
+  test('Should call ProfileReposiory with correct value.', async () => {
     const { profileRepositoryStub, getOneProfileUseCase } = makeSut();
     const getOneSpy = jest.spyOn(profileRepositoryStub, 'getOne');
-    await getOneProfileUseCase.execute(fakeProfile.id, fakeProfile.id);
+    await getOneProfileUseCase.execute(fakeProfile.id, fakeUser.id);
     expect(getOneSpy).toHaveBeenCalledWith(fakeProfile.id);
   });
 
@@ -34,7 +36,7 @@ describe('GetOneProfileUseCase', () => {
     await expect(promise).rejects.toThrow();
   });
 
-  test('Should return void if foundProfile.userId is different than the given user id.', async () => {
+  test('Should throw if foundProfile.userId is different than the given user id.', async () => {
     const { profileRepositoryStub, getOneProfileUseCase } = makeSut();
     jest
       .spyOn(profileRepositoryStub, 'getOne')
@@ -43,22 +45,22 @@ describe('GetOneProfileUseCase', () => {
           resolve({ ...fakeProfile, userId: 'another_id' }),
         ),
       );
-    const profile = await getOneProfileUseCase.execute(
+    const promise = getOneProfileUseCase.execute(
       fakeProfile.id,
       fakeProfile.id,
     );
-    expect(profile).toBeFalsy();
+    await expect(promise).rejects.toThrow(InvalidParamError);
   });
 
-  test('Should return void if foundProfile.getOne return void.', async () => {
+  test('Should throw if foundProfile.getOne return void.', async () => {
     const { profileRepositoryStub, getOneProfileUseCase } = makeSut();
     jest
       .spyOn(profileRepositoryStub, 'getOne')
       .mockReturnValueOnce(new Promise((resolve) => resolve()));
-    const profile = await getOneProfileUseCase.execute(
+    const promise = getOneProfileUseCase.execute(
       fakeProfile.id,
       fakeProfile.id,
     );
-    expect(profile).toBeFalsy();
+    await expect(promise).rejects.toThrow(InvalidParamError);
   });
 });
