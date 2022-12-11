267aca98bd11f9f5e12c5d3bb48953051319c87e
Douglas Volcato <douglasvolcato@gmail.com>
2022-12-11 00:24:44 -0300

    refactor: change LoginAuthUseCase file name

--- src/main/factories/auth-controller-factory.ts
+++ src/main/factories/auth-controller-factory.ts
@@ -1,4 +1,4 @@
-import { LoginAuthUseCase } from 'src/data/useCases/auth/login-user-usecase';
+import { LoginAuthUseCase } from 'src/data/useCases/auth/login-auth-usecase';
 import { UserRepository } from 'src/infra/repositories/user-repository';
 import { AuthControllerInterface } from 'src/presentation/abstract/controllers/auth-controller-interface';
 import { AuthController } from 'src/presentation/controllers/auth/auth-controller';
