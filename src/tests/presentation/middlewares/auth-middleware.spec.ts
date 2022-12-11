7b4291dbeae76e6c0b57f368f1fb2630746f6a40
Douglas Volcato <douglasvolcato@gmail.com>
2022-12-11 19:47:30 -0300

    test: add AuthMiddleware unitary tests

---
+++ src/tests/presentation/middlewares/auth-middleware.spec.ts
@@ -0,0 +1,85 @@
+import { TokenHandlerAdapterStub } from '../../test-utils/stubs/adapters/token-handler-adapter-stub';
+import { AuthMiddleware } from '../../../presentation/middlewares/auth-middleware';
+import { makeHttpRequest } from '../../test-utils/http/make-http-request';
+import { UnauthorizedError } from '../../../utils/errors';
+import { makeError } from '../../test-utils/errors/make-error';
+import { fakeUser } from '../../test-utils/fake-entities/fake-user';
+
+interface SutTypes {
+  tokenHandlerAdapterStub: TokenHandlerAdapterStub;
+  authMiddleware: AuthMiddleware;
+}
+
+function makeSut(): SutTypes {
+  const tokenHandlerAdapterStub = new TokenHandlerAdapterStub();
+  const authMiddleware = new AuthMiddleware(tokenHandlerAdapterStub);
+  return { tokenHandlerAdapterStub, authMiddleware };
+}
+
+describe('AuthMiddleware', () => {
+  test('Should throw if authorization was not informed.', async () => {
+    const { authMiddleware } = makeSut();
+    const promise = authMiddleware.auth(makeHttpRequest({}));
+    await expect(promise).rejects.toThrow(UnauthorizedError);
+  });
+
+  test('Should throw if authorization has not two words.', async () => {
+    const { authMiddleware } = makeSut();
+    const promise = authMiddleware.auth(
+      makeHttpRequest({ headers: { authorization: 'any_token' } }),
+    );
+    await expect(promise).rejects.toThrow(UnauthorizedError);
+  });
+
+  test('Should throw if authorization has not the word Bearer.', async () => {
+    const { authMiddleware } = makeSut();
+    const promise = authMiddleware.auth(
+      makeHttpRequest({ headers: { authorization: 'somer_word any_token' } }),
+    );
+    await expect(promise).rejects.toThrow(UnauthorizedError);
+  });
+
+  test('Should throw if authorization has more than three words.', async () => {
+    const { authMiddleware } = makeSut();
+    const promise = authMiddleware.auth(
+      makeHttpRequest({
+        headers: { authorization: 'some_word any_token another_word' },
+      }),
+    );
+    await expect(promise).rejects.toThrow(UnauthorizedError);
+  });
+
+  test('Should call TokenHandler with correct value.', async () => {
+    const { authMiddleware, tokenHandlerAdapterStub } = makeSut();
+    const validatorSpy = jest.spyOn(tokenHandlerAdapterStub, 'validateToken');
+    await authMiddleware.auth(
+      makeHttpRequest({
+        headers: { authorization: 'Bearer any_token' },
+      }),
+    );
+    expect(validatorSpy).toHaveBeenCalledWith('any_token');
+  });
+
+  test('Should throw is TokenHandler throws.', async () => {
+    const { authMiddleware, tokenHandlerAdapterStub } = makeSut();
+    jest
+      .spyOn(tokenHandlerAdapterStub, 'validateToken')
+      .mockReturnValueOnce(makeError());
+    const promise = authMiddleware.auth(
+      makeHttpRequest({
+        headers: { authorization: 'Bearer any_token' },
+      }),
+    );
+    await expect(promise).rejects.toThrow(UnauthorizedError);
+  });
+
+  test('Should return a user on success.', async () => {
+    const { authMiddleware } = makeSut();
+    const user = await authMiddleware.auth(
+      makeHttpRequest({
+        headers: { authorization: 'Bearer any_token' },
+      }),
+    );
+    expect(user).toBe(fakeUser);
+  });
+});
