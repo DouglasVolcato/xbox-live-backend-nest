2342b3f416a6b5abc91b2a05f859e4c29f01945d
Douglas Volcato <douglasvolcato@gmail.com>
2022-12-11 12:06:39 -0300

    feat: ensure user email will not be registered twice

--- src/data/useCases/user/create-user-usecase.ts
+++ src/data/useCases/user/create-user-usecase.ts
@@ -2,6 +2,7 @@ import { UserDto } from '../../../domain/dtos/user-dto';
 import { UserRepositoryInterface } from '../../../infra/repositories/abstract/user-repository-interface';
 import { CreateUserUseCaseInterface } from '../../../data/abstract/user/create-user-interface';
 import { UserEntity } from '../../../entities/user-entity';
+import { InvalidParamError } from '../../../utils/errors';
 
 export class CreateUserUseCase implements CreateUserUseCaseInterface {
   constructor(private readonly repository: UserRepositoryInterface) {}
@@ -10,6 +11,11 @@ export class CreateUserUseCase implements CreateUserUseCaseInterface {
     const userBody = new UserEntity(body);
     userBody.validateBody();
     const newUser = userBody.getBody();
+
+    const foundUser = await this.repository.getOneByEmail(newUser.email);
+    if (foundUser && foundUser.email === newUser.email) {
+      throw new InvalidParamError('Email already registered.');
+    }
 
     const createdUser = await this.repository.create(newUser);
     if (createdUser) {
